82ef3ad22d46378d8a49db66721115c9ee4feba9
ifgabriel <gabrielmirandap.ti@gmail.com>
2023-08-01 12:01:49 -0300

    feat(orsegups): create initial server

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import routes from "./routes";
+
+const app = express();
+const port = 3003;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/", routes);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
